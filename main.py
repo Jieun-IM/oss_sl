e71bb14637d709f01b7003e63f29db58d4841864
Jieun Im <126746083+Jieun-IM@users.noreply.github.com>
2023-05-22 23:01:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ import pandas as pd
 import graphviz as graphviz
 
 st.title("Total_Investment")
-st.subheader("IMF, 2021~2023, 총투자")
+st.subheader("IMF, 2021~2023, 총투자")  # https://kosis.kr/statHtml/statHtml.do?orgId=101&tblId=DT_2WEO009&vw_cd=MT_RTITLE&list_id=R_SUB_OTITLE_OTIT_IMFTIT_001&scrId=&seqNo=&lang_mode=ko&obj_var_id=&itm_id=&conn_path=MT_RTITLE&path=%252FstatisticsList%252FstatisticsListIndex.do
+
