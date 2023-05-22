aac7470cacb15ee3c3e705f7a3e52115932e800a
Jieun Im <126746083+Jieun-IM@users.noreply.github.com>
2023-05-22 23:09:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.title("Total_Investment")
 st.subheader("IMF, 2021~2023, 총투자")  # https://kosis.kr/statHtml/statHtml.do?orgId=101&tblId=DT_2WEO009&vw_cd=MT_RTITLE&list_id=R_SUB_OTITLE_OTIT_IMFTIT_001&scrId=&seqNo=&lang_mode=ko&obj_var_id=&itm_id=&conn_path=MT_RTITLE&path=%252FstatisticsList%252FstatisticsListIndex.do
 
 df = pd.read_csv('./data/Total_Investment_20230522224509.csv')
-df.set_index = df['index']
+df.set_index = df['Country']
 
 if st.button('data copyright link'):
     st.write('https://kosis.kr/statHtml/statHtml.do?orgId=101&tblId=DT_2WEO009&vw_cd=MT_RTITLE&list_id=R_SUB_OTITLE_OTIT_IMFTIT_001&scrId=&seqNo=&lang_mode=ko&obj_var_id=&itm_id=&conn_path=MT_RTITLE&path=%252FstatisticsList%252FstatisticsListIndex.do')
